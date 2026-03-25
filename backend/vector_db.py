12d264677ed5f62e149e8ea826191297cd0c9deb
Sachin <sachhaya1@gmail.com>
2026-03-25 17:40:55 -0400

    began vector_db file

---
+++ backend/vector_db.py
@@ -0,0 +1,10 @@
+import chromadb 
+from PIL import Image
+from transformers import CLIPProcessor, CLIPModel
+import torch 
+
+def crop_image(image, box): 
+    #image is a PIL Image object
+    # box is [x1,x2, y1, y2] in pixel coordinates
+    #return the cropped image\
+    pass
